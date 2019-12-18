98928c7ea47e8907b5c9753358246b7dc341b80d
Christopher Lemaitre <kriss7670@gmail.com>
2019-12-18 16:52:33 +0100

    teste

--- includes/accueil.inc.php
+++ includes/accueil.inc.php
@@ -1,1 +1,9 @@
-<h1>Accueil</h1>
+<h1>Bienvenue dans mon monde</h1>
+<section class="orange">
+      <div class="leftexp">
+        <p>Teste Zombies</p>
+          </div>
+      <div class="rightexp">
+        <p>ceci est un teste de zombies</p>
+      </div>
+</section>
--- includes/presentation.inc.php
+++ includes/presentation.inc.php
@@ -1,1 +1,12 @@
-<h1>Presentation</h1>
+<section class="Presentation">
+    <h1> Qui Suis-Je ??</h1>
+      <div class="sec">
+          <div class="left">
+              <p>Photo a venir</p>
+                  </div>
+          <div class="right">
+              <p>Je m'appelle Kris,j'ai 33ans et je vis à RouenZombie</p>
+              <p>Je suis un grand fan de jeux vidéos, informatique</p>
+          </div>
+      </div>
+</section>
