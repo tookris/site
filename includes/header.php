a57e65f1fda46c4f94db43e249569cf198ec0afe
Christopher Lemaitre <kriss7670@gmail.com>
2019-12-18 16:00:43 +0100

    mise a jour nav

--- includes/header.php
+++ includes/header.php
@@ -2,11 +2,16 @@
   <h1>Le site de Kris</h1>
   <nav>
     <ul>
-      <li><a href="index.php?page=accueil" title="Page d'accueil">Accueil</a></li>
-      <li><a href="index.php?page=presentation" title="Presentation">Presentation</a></li>
-      <li><a href="index.php?page=cv" title="CV">Mon CV</a></li>
-      <li><a href="index.php?page=contact" title="Nous contacter">Contact</a></li>
-      <li><a href="index.php?page=mentions" title="Les mentions légales">Mentions légales</a></li>
+      <li  class="menu-acc">
+        <a href="index.php?page=accueil" title="Page d'accueil">Accueil</a></li>
+      <li  class="menu-pre">
+        <a href="index.php?page=presentation" title="Presentation">Presentation</a></li>
+      <li  class="menu-cv">
+        <a href="index.php?page=cv" title="CV">Mon CV</a></li>
+      <li  class="menu-con">
+        <a href="index.php?page=contact" title="Nous contacter">Contact</a></li>
+      <li  class="menu-men">
+        <a href="index.php?page=mentions" title="Les mentions légales">Mentions légales</a></li>
     </ul>
   </nav>
 </header>
