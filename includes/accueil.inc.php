ce08cac7be7519415a66bbee3dbaaf14fa577c86
Christopher Lemaitre <kriss7670@gmail.com>
2020-01-10 16:50:33 +0100

    indentation globale

--- includes/accueil.inc.php
+++ includes/accueil.inc.php
@@ -1,37 +1,43 @@
 <div class="header">
-  <div id="teste">
-    <h1>Bienvenue Jeune Zombie</h1>
-  </div>
+	<div id="teste">
+		<h1>Bienvenue Jeune Zombie</h1>
+	</div>
 </div>
-
 <div class="row">
-  <div class="column">
-    <h2>Zombie 1</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
-  </div>
-
-  <div class="column">
-    <h2>Zombie 2</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
-  </div>
-
-  <div class="column">
-    <h2>Zombie 3</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
-  </div>
-
-  <div class="column">
-    <h2>Zombie 4</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
-  </div>
-
-  <div class="column">
-    <h2>Zombie 5</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
-  </div>
-
-  <div class="column">
-    <h2>Zombie 6</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
-  </div>
+	<div class="column">
+		<h2>Zombie 1</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.
+		</p>
+	</div>
+	<div class="column">
+		<h2>Zombie 2</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.
+		</p>
+	</div>
+	<div class="column">
+		<h2>Zombie 3</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.
+		</p>
+	</div>
+	<div class="column">
+		<h2>Zombie 4</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.
+		</p>
+	</div>
+	<div class="column">
+		<h2>Zombie 5</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.
+		</p>
+	</div>
+	<div class="column">
+		<h2>Zombie 6</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.
+		</p>
+	</div>
 </div>
--- includes/mentions.inc.php
+++ includes/mentions.inc.php
@@ -1,11 +1,14 @@
 <div id="teste">
-  <h1>Mentions Légales</h1>
+	<h1>Mentions Légales</h1>
 </div>
-
 <section class="mentions">
-
-  <abbr title="Société à responsabilité limitée"> S.A.R.L Crazy Site </abbr>
-    <p> 142 rue des beauf, 76000 Rouen</p>
-  <abbr title="Chiffre d'affaires"> C.A 1000000€ </abbr>
-
+	<abbr title="Société à responsabilité limitée">
+		 S.A.R.L Crazy Site
+	</abbr>
+	<p>
+		 142 rue des beauf, 76000 Rouen
+	</p>
+	<abbr title="Chiffre d'affaires">
+		 C.A 1000000€
+	</abbr>
 </section>
